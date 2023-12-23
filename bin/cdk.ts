1c7d6aba32e7f65bc21fdcbac4bcd8963a4bdbd1
Isaac Graves <igraves@hbs.edu>
2023-12-22 19:49:26 -0800

    Add cdk region

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -5,4 +5,8 @@ import { Stack } from "./stack";
 
 const app = new cdk.App();
 
-new Stack(app, "StacklineTakehome");
+new Stack(app, "StacklineTakehome", {
+  env: {
+    region: "us-east-1"
+  }
+});
