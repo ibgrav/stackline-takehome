e15bfd54d80b0eea9c94bf028272fa95e7ba1b1e
Isaac Graves <igraves@hbs.edu>
2023-12-22 17:24:47 -0800

    add deploy

--- bin/stack.ts
+++ bin/stack.ts
@@ -1,15 +1,22 @@
+import { resolve } from "node:path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "StacklineTakehomeBucket", {
-      publicReadAccess: true,
+    const bucket = new s3.Bucket(this, `${id}Bucket`, {
+      accessControl: s3.BucketAccessControl.PUBLIC_READ,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: "index.html"
+    });
+
+    new s3Deployment.BucketDeployment(this, `${id}BucketDeployment`, {
+      destinationBucket: bucket,
+      sources: [s3Deployment.Source.asset(resolve(process.cwd(), "dist"))]
     });
   }
 }
