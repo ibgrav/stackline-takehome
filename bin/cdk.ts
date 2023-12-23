cf7a5234c3d12dd86d21f3d7d78337e1986e9be0
Isaac Graves <igraves@hbs.edu>
2023-12-23 05:50:51 -0800

    Simplify aws cdk deploy

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -5,9 +5,4 @@ import { Stack } from "./stack";
 
 const app = new cdk.App();
 
-new Stack(app, "StacklineTakehome", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  }
-});
+new Stack(app, "StacklineTakehome");
--- bin/stack.ts
+++ bin/stack.ts
@@ -2,15 +2,12 @@ import { resolve } from "node:path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
 
-const rootDomain = "isaac.works";
-const stacklineDomain = `stackline.${rootDomain}`;
+const domainName = `stackline.isaac.works`;
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,20 +21,16 @@ export class Stack extends cdk.Stack {
 
     bucket.grantRead(originAccessIdentity);
 
-    const hostedZone = route53.PublicHostedZone.fromLookup(this, `${id}HostedZone`, {
-      domainName: rootDomain
-    });
-
     const certificate = new acm.Certificate(this, `${id}Certificate`, {
-      domainName: stacklineDomain,
-      validation: acm.CertificateValidation.fromDns(hostedZone)
+      domainName,
+      validation: acm.CertificateValidation.fromDns()
     });
 
     const distribution = new cloudfront.Distribution(this, `${id}Distribution`, {
       defaultRootObject: "index.html",
       httpVersion: cloudfront.HttpVersion.HTTP2,
       priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-      domainNames: [stacklineDomain],
+      domainNames: [domainName],
       certificate: certificate,
       defaultBehavior: {
         origin: new cloudfrontOrigins.S3Origin(bucket, { originAccessIdentity }),
@@ -45,12 +38,6 @@ export class Stack extends cdk.Stack {
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED
       }
-    });
-
-    new route53.ARecord(this, `${id}ARecord`, {
-      zone: hostedZone,
-      recordName: stacklineDomain,
-      target: route53.RecordTarget.fromAlias(new route53Targets.CloudFrontTarget(distribution))
     });
 
     new s3Deployment.BucketDeployment(this, `${id}BucketDeployment`, {
