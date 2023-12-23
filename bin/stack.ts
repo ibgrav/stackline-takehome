e12b179346bb51aac53d0a0844690756cc821007
Isaac Graves <igraves@hbs.edu>
2023-12-22 17:37:20 -0800

    Add cloudfront

--- bin/stack.ts
+++ bin/stack.ts
@@ -2,6 +2,8 @@ import { resolve } from "node:path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 
 export class Stack extends cdk.Stack {
@@ -9,7 +11,6 @@ export class Stack extends cdk.Stack {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, `${id}Bucket`, {
-      publicReadAccess: true,
       websiteIndexDocument: "index.html",
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
@@ -17,6 +18,17 @@ export class Stack extends cdk.Stack {
     new s3Deployment.BucketDeployment(this, `${id}BucketDeployment`, {
       destinationBucket: bucket,
       sources: [s3Deployment.Source.asset(resolve(process.cwd(), "dist"))]
+    });
+
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, `${id}OriginAccessIdentity`);
+
+    bucket.grantRead(originAccessIdentity);
+
+    new cloudfront.Distribution(this, `${id}Distribution`, {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new cloudfrontOrigins.S3Origin(bucket, { originAccessIdentity })
+      }
     });
   }
 }
