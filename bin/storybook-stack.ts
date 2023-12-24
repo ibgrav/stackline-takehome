45ca1d9be83c5904bb03eb061d80fe72c86a7384
Isaac Graves <igraves@hbs.edu>
2023-12-24 07:40:25 -0800

    switch to cloudfront

--- bin/storybook-stack.ts
+++ bin/storybook-stack.ts
@@ -2,34 +2,45 @@ import { resolve } from "node:path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
+
+export const storybookAssetDir = resolve(process.cwd(), "dist");
 
 const domainName = "stackline-storybook.isaac.works";
 
-export const storybookAssetDir = resolve(process.cwd(), "storybook-static");
-
+// see ./site-stack.ts for a more detailed explanation of this code
 export class StorybookStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // public s3 bucket to serve the site
-    // the bucket name must match the domain name for the CNAME to work
-    const bucket = new s3.Bucket(this, `${id}Bucket`, {
-      bucketName: domainName,
-      publicReadAccess: true,
-      websiteIndexDocument: "index.html",
-      websiteErrorDocument: "index.html",
-      autoDeleteObjects: true,
-      enforceSSL: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL
+    const bucket = new s3.Bucket(this, `${id}Bucket`);
+
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, `${id}OriginAccessIdentity`);
+
+    bucket.grantRead(originAccessIdentity);
+
+    const certificate = new acm.Certificate(this, `${id}Certificate`, {
+      domainName,
+      validation: acm.CertificateValidation.fromDns()
     });
 
-    // deploy the built assets to the s3 bucket
+    const distribution = new cloudfront.Distribution(this, `${id}Distribution`, {
+      defaultRootObject: "index.html",
+      domainNames: [domainName],
+      certificate: certificate,
+      defaultBehavior: {
+        origin: new cloudfrontOrigins.S3Origin(bucket, { originAccessIdentity })
+      }
+    });
+
     new s3Deployment.BucketDeployment(this, `${id}BucketDeployment`, {
       sources: [s3Deployment.Source.asset(storybookAssetDir)],
-      destinationBucket: bucket
+      destinationBucket: bucket,
+      distribution, // invalidate all paths
+      distributionPaths: ["*"]
     });
   }
 }
