0903ae15f98ea7032334665f3e1dd5295e35fe0f
Isaac Graves <igraves@hbs.edu>
2023-12-24 06:04:12 -0800

    extract font files

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ export function Header() {
     <header className="bg-primary shadow-sm">
       <div className="flex p-4">
         {/* eager loading for an image above-the-fold, for better core web vital performance */}
-        <img className="w-40" src="/assets/stackline_logo.svg" loading="eager" />
+        <img className="w-40" src="/assets/media/stackline_logo.svg" loading="eager" />
       </div>
     </header>
   );
