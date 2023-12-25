e9b3913ed180876973abf61399ac5d9b32be94f7
Isaac Graves <igraves@hbs.edu>
2023-12-25 06:59:27 -0800

    Add logo to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,10 @@
-// import { Logo } from "@/components/logo/logo";
+import { Logo } from "@/components/logo/logo";
 
 export function Header() {
   return (
     <header className="bg-primary shadow-sm">
       <div className="flex container py-4">
-        {/* eager loading for an image in FCP, for better core web vital performance */}
-        {/* <Logo className="w-40" pathClassName="fill-white" /> */}
-        <img className="w-40" src="/assets/media/stackline_logo.svg" loading="eager" alt="Stackline Logo" />
+        <Logo className="w-40" pathClassName="fill-white" />
         <h1 className="sr-only">Stackline Takehome</h1>
       </div>
     </header>
